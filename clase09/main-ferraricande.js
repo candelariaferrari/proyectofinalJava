e09418cefc9b48c19661d2c107ef6ab950c9fdcf
Cande Ferrari <cande@gmail.com>
2021-02-21 22:45:22 -0300

    .

--- clase09/main-ferraricande.js
+++ clase09/main-ferraricande.js
@@ -1,5 +1,5 @@
 //primer paso - asosciar evento (en cual lo escucho)
-let miBoton = document.getElementById("btnPrueba");
+//let miBoton = document.getElementById("btnPrueba");
 // segundo paso - asociar el evento al manejador de eventos
 /* // OPCION 1
 miBoton.onclick = function(){
@@ -8,15 +8,30 @@ miBoton.onclick = function(){
 // OPCION 2
 miBoton.addEventListener('click', ()=> {
     alert(" respuesta desde addEvent ");
-})*/
+})
 // OPCION DESDE VARIABLE 
 let eventoNombre = 'click';
 miBoton.addEventListener(eventoNombre, ()=>{
     alert(" respuesta desde variable  ");
 
 })
-console.dir(miBoton);
+console.dir(miBoton);*/
 
+/* let  formulario = document.getElementById("pruebaFormulario");
+formulario.onsubmit = (event) => {
+    event.preventDefault (); // evita el comportamiento por defecto del form de "volver a cargar la pag"
+    // para enviar la info del formulario 
+    let dataFormulario = {
+        nombre: event.target.children[0].value,
+        precio: event.target.children[1].value
+    }
+    let padre = document.getElementById("notificacion"); // salida 
+    padre.innerHTML = `agregado producto ${dataFormulario.nombre}`;
+    console.log(dataFormulario); // para ver la info del formulario 
+    /* console.log(event.target.children[0].value); // "text" en la consola me muestra los valores obtenidos
+    console.log(event.target.children[1].value);// "number" en la consola me muestra los valores obtenidos
+
+} */
 class Producto{
     constructor(datosProductos){
         this.id     = datosProductos.id;
@@ -28,14 +43,6 @@ class Producto{
         this.precio -= agregado;
     }
 }
-/* let listaProductos = [];
-listaProductos.push(new Producto(datosProductos[0]));
-listaProductos.push(new Producto(datosProductos[1]));
-listaProductos.push(new Producto(datosProductos[2]));
-listaProductos.push(new Producto(datosProductos[3]));
-listaProductos.push(new Producto(datosProductos[4]));
-listaProductos.push(new Producto(datosProductos[5]));
-listaProductos.push(new Producto(datosProductos[6])); */
 
 let   productosSeleccionados = [];
 const PREFIJO                = "productoID";
@@ -51,12 +58,12 @@ for(let dato of datosProductos){
 function crearElemento(dato){
     let nuevoElemento = document.createElement("div");
     nuevoElemento.id        = PREFIJO + dato.id;
-    nuevoElemento.innerHTML = `<h3> ${dato.nombre}</h3>
+    nuevoElemento.innerHTML = `<h3 class="nombre"> ${dato.nombre}</h3>
                               <h4>${dato.precio}</h4>
-                              <button id="${dato.id}"> comprar </button>`;
+                              <button id="${dato.nombre}"> comprar </button>`;
 
     padre.appendChild(nuevoElemento);
-    let boton = document.getElementById(dato.id);
+    let boton = document.getElementById(dato.nombre);
     boton.onclick = () => { // para que se vea el nombre del producto " comprado "
         alert ("producto vendido " + dato.nombre)
     };
